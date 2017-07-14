c3d05279e3aeb2853b15f7599acc8e5b619d124c
Yuusaku Miyazaki <toumin.m7@gmail.com>
2017-07-14 11:18:53 +0900

    Use latest method instead of orderBy.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,7 +19,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::orderBy('created_at', 'desc')->paginate(5);
+        $posts = Post::latest()->paginate(5);
         return view('posts.index', ['posts' => $posts]);
     }
 

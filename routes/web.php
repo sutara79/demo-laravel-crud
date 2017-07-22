c5db56e9b5cdef751ff4df2b70c4041b9ac4d125
Yuusaku Miyazaki <toumin.m7@gmail.com>
2017-07-22 17:38:02 +0900

    Add predis.

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,11 @@ Route::group(['middleware' => 'locale'], function () {
     Route::get('foo/foo3', 'FooController@foo3');
     Route::get('foo/foo4', 'FooController@foo4');
 
+    Route::get('test-session', function () {
+        session(['foo' => 'こんにちはこんにちは!!']);
+        return session('foo');
+    });
+
     // Auth
     Auth::routes();
 

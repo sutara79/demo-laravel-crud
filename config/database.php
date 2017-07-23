530953389d074c932b5e43bdbabb4c9d573441c1
Yuusaku Miyazaki <toumin.m7@gmail.com>
2017-07-23 17:44:58 +0900

    Add config for Heroku.

--- config/database.php
+++ config/database.php
@@ -1,4 +1,12 @@
 <?php
+if (getenv('REDIS_URL')) {
+    $url = parse_url(getenv('REDIS_URL'));
+
+    putenv('REDIS_HOST='.$url['host']);
+    putenv('REDIS_PORT='.$url['port']);
+    putenv('REDIS_PASSWORD='.$url['pass']);
+}
+
 
 return [
 

f1b165ab7dfbf649710ffe3cb4771d4c48a85147
Yuusaku Miyazaki <toumin.m7@gmail.com>
2017-10-13 09:09:58 +0900

    Not to edit email

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -14,13 +14,6 @@
             <span class="invalid-feedback">{{ $errors->first('name') }}</span>
         @endif
     </div>
-    <div class="form-group">
-        <label for="email">{{ __('Email') }}</label>
-        <input id="email" type="email" class="form-control @if ($errors->has('email')) is-invalid @endif" name="email" value="{{ old('email', $user->email) }}" required>
-        @if ($errors->has('email'))
-            <span class="invalid-feedback">{{ $errors->first('email') }}</span>
-        @endif
-    </div>
     <button type="submit" name="submit" class="btn btn-primary">{{ __('Submit') }}</button>
 </form>
 @endsection

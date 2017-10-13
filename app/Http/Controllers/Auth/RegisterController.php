3596fc16ab3a63ae20cfbb66f025d00cd4842358
Yuusaku Miyazaki <toumin.m7@gmail.com>
2017-10-13 09:09:34 +0900

    Integrate validation rule

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,11 +48,12 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
+        // return Validator::make($data, [
+        //     'name' => 'required|string|max:255',
+        //     'email' => 'required|string|email|max:255|unique:users',
+        //     'password' => 'required|string|min:6|confirmed',
+        // ]);
+        return Validator::make($data, (new \App\Http\Requests\StoreUser())->rules());
     }
 
     /**

7bb2422793ebc76f89f2e6cd2020ef2d355ebf32
Yuusaku Miyazaki <toumin.m7@gmail.com>
2018-02-25 15:18:10 +0900

    Modify calling seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(PostsTableSeeder::class);
+        $this->call([
+            UsersTableSeeder::class,
+            PostsTableSeeder::class,
+        ]);
     }
 }

8a43ea1e2c5bf11a699804425861f77df4693a43
Yuusaku Miyazaki <toumin.m7@gmail.com>
2018-03-09 13:46:40 +0900

    Modify error-page

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -3,7 +3,9 @@
 @endphp
 @extends('layouts.my')
 @section('content')
-<h1>{{ $title }}</h1>
-<p><strong>{{ __('Error') }}: <span class="error-code">403</span></strong></p>
-<p>{{ __('You do not have permission to access this page.') }}</p>
+<div class="container">
+    <h1>{{ $title }}</h1>
+    <p><strong>{{ __('Error') }}: <span class="error-code">403</span></strong></p>
+    <p>{{ __('You do not have permission to access this page.') }}</p>
+</div>
 @endsection
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,7 +3,9 @@
 @endphp
 @extends('layouts.my')
 @section('content')
-<h1>{{ $title }}</h1>
-<p><strong>{{ __('Error') }}: <span class="error-code">500</span></strong></p>
-<p>{{ __('The server was unable to complete your request.') }}</p>
+<div class="container">
+    <h1>{{ $title }}</h1>
+    <p><strong>{{ __('Error') }}: <span class="error-code">500</span></strong></p>
+    <p>{{ __('The server was unable to complete your request.') }}</p>
+</div>
 @endsection

dc3ea72b5ac9ee9b5a81f9f0bf1aa1ed05759a16
Yuusaku Miyazaki <toumin.m7@gmail.com>
2018-03-12 16:16:04 +0900

    Register DuskServiceProvider exept for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Dusk\DuskServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,12 +14,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // MySQL5.7.7、またはMariaDB10.2.2より古い場合、varchar型の文字数を191に制限する
+        // See https://laravel.com/docs/5.6/migrations#indexes
         \Schema::defaultStringLength(191);
 
-        // Set global variable.
+        // グローバル変数を設定する
         // See https://stackoverflow.com/questions/28356193/
         config(['admin_id' => 1]); // Use at app/User.php
 
+        // HerokuでHTTPSを強制する
         if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
@@ -31,6 +35,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // 本番環境以外で、Duskサービスプロバイダを登録する
+        if ($this->app->environment('local', 'testing')) {
+            $this->app->register(DuskServiceProvider::class);
+        }
     }
 }

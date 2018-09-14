99424a107a5d0ba12ed368344212d463490920d8
Yuusaku Miyazaki <toumin.m7@gmail.com>
2018-09-14 19:12:29 +0900

    Force https on heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
         // 管理者のID番号を1とする
         // 参照: https://stackoverflow.com/questions/28356193/
         config(['admin_id' => 1]);
+
+        // 本番環境(Heroku)でhttpsを強制する
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 
     /**

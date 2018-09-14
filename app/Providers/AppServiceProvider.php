b4c0ad2f297505b33dcdc4352a15ecae13161eab
Yuusaku Miyazaki <toumin.m7@gmail.com>
2018-09-14 17:21:58 +0900

    Import DuskServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Laravel\Dusk\DuskServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
